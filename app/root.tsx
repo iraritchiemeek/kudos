390b525362136e22f1141e94e89836494a4abdb7
iraritchiemeek <iraritchiemeek@gmail.com>
2022-05-23 14:41:27 +0900

    initial commit. Set up mongodb

---
+++ app/root.tsx
@@ -0,0 +1,33 @@
+import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import styles from './styles/app.css';
+
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
